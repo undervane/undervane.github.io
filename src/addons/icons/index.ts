4f187dbb89933bdbfa62223530ff7ce57c6cc8de
Undervane <sergio@mipigu.com>
2019-06-03 21:57:40 +0200

    Reduced even more bundle size

---
+++ src/addons/icons/index.ts
@@ -0,0 +1,7 @@
+export {
+	default as CheckCircleFill
+} from '@ant-design/icons/lib/fill/CheckCircleFill';
+
+export {
+	default as ExclamationCircleFill
+} from '@ant-design/icons/lib/fill/ExclamationCircleFill';
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,12 @@
+var path = require('path');
+
 module.exports = {
-  productionSourceMap: false
+  productionSourceMap: false,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@ant-design/icons/lib/dist$': path.resolve(__dirname, './src/addons/icons')
+      }
+    }
+  }
 };
